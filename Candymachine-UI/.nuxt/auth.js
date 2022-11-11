5e3eac5586ea115ba4f1d0f6b59f063a74c64f95
Pashupati Sah <pashupatisah112@gmail.com>
2022-11-11 10:19:40 +0545

    added candy machine ui

---
+++ Candymachine-UI/.nuxt/auth.js
@@ -0,0 +1,60 @@
+import Middleware from './middleware'
+import { Auth, authMiddleware, ExpiredAuthSessionError } from '~auth/runtime'
+
+// Active schemes
+
+Middleware.auth = authMiddleware
+
+export default function (ctx, inject) {
+  // Options
+  const options = {
+  "resetOnError": false,
+  "ignoreExceptions": false,
+  "scopeKey": "scope",
+  "rewriteRedirects": true,
+  "fullPathRedirect": false,
+  "watchLoggedIn": true,
+  "redirect": {
+    "login": "/login",
+    "logout": "/",
+    "home": "/",
+    "callback": "/login"
+  },
+  "vuex": {
+    "namespace": "auth"
+  },
+  "cookie": {
+    "prefix": "auth.",
+    "options": {
+      "path": "/"
+    }
+  },
+  "localStorage": {
+    "prefix": "auth."
+  },
+  "defaultStrategy": ""
+}
+
+  // Create a new Auth instance
+  const $auth = new Auth(ctx, options)
+
+  // Register strategies
+
+  // Inject it to nuxt context as $auth
+  inject('auth', $auth)
+  ctx.$auth = $auth
+
+  // Initialize auth
+  return $auth.init().catch(error => {
+    if (process.client) {
+      // Don't console log expired auth session errors. This error is common, and expected to happen.
+      // The error happens whenever the user does an ssr request (reload/initial navigation) with an expired refresh
+      // token. We don't want to log this as an error.
+      if (error instanceof ExpiredAuthSessionError) {
+        return
+      }
+
+      console.error('[ERROR] [AUTH]', error)
+    }
+  })
+}
